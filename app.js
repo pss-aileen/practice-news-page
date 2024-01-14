3bbfa554b8380f8479e7d431317761daa6ccdbd1
pss-aileen <pss.aiando@gmail.com>
2024-01-14 14:25:16 +0800

    💕 FEAT: dotenv追加、databaseフォルダ作成、リファクタリング

--- app.js
+++ app.js
@@ -6,13 +6,24 @@
   const newsRoute = require("./routes/news");
   const PORT = 5050;
   app.use(express.json());
+  require("dotenv").config();
 
 
   app.listen(PORT, () => {
     console.log("サーバーを起動しています");
   })
 
-
   app.use("/api/v1/news", newsRoute);
+
+
+  const mongoose = require("mongoose");
+
+  main().catch(err => console.log(err));
+
+  async function main() {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("データベースに接続しています");
+  }
+
 
 }
