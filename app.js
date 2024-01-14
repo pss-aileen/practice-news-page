09dcb9eac2e86fe34948d8b1d482184e3f407f41
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2024-01-14 14:35:26 +0800

    💕 FEAT: listenの位置変更※こちらが正しい

--- app.js
+++ app.js
@@ -5,20 +5,21 @@
   const app = express();
   const newsRoute = require("./routes/news");
   const connectDB = require("./database/connect");
-  const PORT = 5050;
   app.use(express.json());
   require("dotenv").config();
+  
+  const PORT = 5050;
 
-
-  app.listen(PORT, () => {
-    console.log("サーバーを起動しています");
-  })
-
+  
+  app.use("/api/v1/news", newsRoute);
   
   const start = async () => {
     try {
       await connectDB(process.env.MONGODB_URL);
-      app.use("/api/v1/news", newsRoute);
+      
+      app.listen(PORT, () => {
+        console.log("サーバーを起動しています");
+      })
     } catch (err) {
       console.log(err);
     }
