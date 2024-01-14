c864cf72f9b091b77d6423d7a0407f192065ef5c
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2024-01-14 16:09:56 +0800

    💕 FEAT: データベースとデータやり取り機能追加
    createNewsとgetSingleNewsまで

--- controllers/news.js
+++ controllers/news.js
@@ -1,16 +1,36 @@
 "use strict";
 {
 
-  const getAllNews = (req, res) => {
-    res.send("ニュースを全て取得しました");
+  const News = require("../models/News");
+
+  const getAllNews = async (req, res) => {
+    // res.send("ニュースを全て取得しました");
+    try {
+      const allNews = await News.find({});
+      res.status(200).json(allNews);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   };
 
-  const createNews = (req, res) => {
-    res.send("ニュースを投稿しました");
+  const createNews = async (req, res) => {
+    // res.send("ニュースを投稿しました");
+    try {
+      const createNews = await News.create(req.body);
+      res.status(200).json(createNews);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   };
 
-  const getSingleNews = (req, res) => {
-    res.send("特定のニュースを取得しました");
+  const getSingleNews = async (req, res) => {
+    // res.send("特定のニュースを取得しました");
+    try {
+      const singleNews = await News.findOne({_id: req.params.id});
+      res.status(200).json(singleNews);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   };
 
   const updateNews = (req, res) => {
---
+++ models/News.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const NewsSchema = new mongoose.Schema({
+  // id, title, date, 編集日 content
+  title: {
+    type: String,
+    required: [true, "お知らせのタイトルを入力してください"],
+    trim: true,
+    maxLength: [100, "お知らせのタイトルは100文字以内で入力してください"]
+  },
+  createdAt: {
+    type: Date,
+    // required: true,
+    default: Date.now
+  },
+  modifiedAt: {
+    type: Date,
+    default: Date.now
+  },
+  content: {
+    type: String,
+    required: true,
+    maxLength: [1000, "お知らせの内容は1000文字以内で入力してください"]
+  }
+});
+
+module.exports = mongoose.model("News", NewsSchema);
+
+// 内容が変わっていなければ変更しないにしないと、cratedAtは基本変更しないようにする
