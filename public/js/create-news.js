72d990fd439833211393d27ee16099a984723784
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2024-01-16 10:45:41 +0800

    💕 FEAT: 細かいCSS、JavaScripを追加

--- public/js/create-news.js
+++ public/js/create-news.js
@@ -4,29 +4,43 @@
 
   console.log(axios);
 
+  const newsSectionDOM = document.querySelector(".create-news");
   const titleDOM = document.querySelector(".create-news__input");
   const cetegoryDOM = document.querySelector(".create-news__select");
   const contentDOM = document.querySelector(".create-news__textarea");
 
-  const createNews = async () => {
+  const createNews = async (published) => {
     try {
       const news = {
         title: titleDOM.value,
         category: cetegoryDOM.value,
-        content: contentDOM.value
+        content: contentDOM.value,
+        published: published
       }
-      await axios.post("api/v1/news", news);
-      console.log(news);
+      const savedNews = await axios.post("api/v1/news", news);
+      
+      while (newsSectionDOM.firstChild) {
+        newsSectionDOM.removeChild(newsSectionDOM.firstChild);
+      }
+      newsSectionDOM.textContent = `「${news.title}」を${published ? "公開しました。" : "下書きとして保存しました。"}`;
+      console.log(savedNews);
       } catch (err) {
         console.log(err);
       }
   }
 
-  const createBtn = document.querySelector(".create-news__btn");
+  const createBtn = document.querySelector(".create-news__btn--create");
 
   createBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    createNews();
+    createNews(true);
+  });
+
+  const saveBtn = document.querySelector(".create-news__btn--draft");
+
+  saveBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    createNews(false);
   });
 
 }
